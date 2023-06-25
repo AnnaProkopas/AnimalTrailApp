080a4b7cfbd6e3a968b9224f5c7d4341d63df0e0
AnnaProkopas <anechkaprokopas@gmail.com>
2023-06-25 11:48:19 +1000

    Add awards screen

---
+++ Assets/Scripts/Awards/AwardCell.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UI.Localization;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Awards
+{
+    public class AwardCell: MonoBehaviour
+    {
+        public delegate void OpenAward(Sprite image, string text);
+        
+        [SerializeField] public AwardType type;
+        [SerializeField] private Sprite emptySprite;
+        [SerializeField] private Sprite disabledSprite;
+        [SerializeField] private Sprite mainSprite;
+
+        public OpenAward openAward;
+        
+        private int count = 0;
+
+        private void OnEnable()
+        {
+            gameObject.GetComponent<Button>().onClick.AddListener(() => OnClick());
+        }
+
+        public void SetEnabled(int count)
+        {
+            GetComponent<Image>().sprite = mainSprite;
+            this.count = count > 0 ? count : 1;
+        }
+        
+        public void SetDisabled()
+        {
+            GetComponent<Image>().sprite = emptySprite;
+        }
+
+        public bool isEnabled()
+        {
+            return count > 0;
+        }
+
+        public void OnClick()
+        {
+            if (count > 0)
+            {
+                string text = LangManager.GetTranslate("Awards_history_" + Enum.GetName(typeof(AwardType), type) + "_key");
+                openAward.Invoke(mainSprite, text);
+            }
+            else
+            {
+                string text = LangManager.GetTranslate("Awards_hint_" + Enum.GetName(typeof(AwardType), type) + "_key");
+                openAward.Invoke(disabledSprite, text);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Awards/AwardsTable.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Awards
+{
+    public class AwardsTable : MonoBehaviour
+    {
+        [SerializeField] private GameObject openedAwardSprite;
+        [SerializeField] private GameObject openedAwardText;
+        private void Awake()
+        {
+            Dictionary<AwardType, StoredAward> awards = AwardsStorage.Load().ToDictionary(x => x.type, x => x);
+            
+            AwardCell[] children = GetComponentsInChildren<AwardCell>();
+
+            foreach (var cell in children)
+            {
+                if (awards.ContainsKey(cell.type))
+                {
+                    cell.SetEnabled(awards[cell.type].count);
+                }
+                else
+                {
+                    cell.SetDisabled();
+                }
+
+                cell.openAward += OpenAward;
+            }
+        }
+
+        void OpenAward(Sprite sprite, string text)
+        {
+            gameObject.SetActive(false);
+            
+            openedAwardSprite.GetComponent<Image>().sprite = sprite;
+            openedAwardSprite.SetActive(true);
+            
+            openedAwardText.GetComponentInChildren<TextMeshProUGUI>().text = text;
+            // var verticalLayoutGroup = openedAwardText.GetComponent<VerticalLayoutGroup>();
+            openedAwardText.SetActive(true);
+            LayoutRebuilder.ForceRebuildLayoutImmediate(openedAwardText.GetComponent<RectTransform>());
+            // openedAwardText.GetComponent<>().ForceRebuildLayoutImmediate;
+        }
+
+        public void OnCloseAward()
+        {
+            openedAwardSprite.SetActive(false);
+            openedAwardText.gameObject.SetActive(false);
+            
+            gameObject.SetActive(true);
+        }
+    }
+}
